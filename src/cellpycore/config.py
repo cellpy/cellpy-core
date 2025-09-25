56cce3d5e6aeca62fd8987ab816f99860f7523dc
jepegit <jepe@ife.no>
2025-09-25 14:21:26 +0200

    tentative enum structure for cols

--- src/cellpycore/config.py
+++ src/cellpycore/config.py
@@ -9,137 +9,165 @@ the settings. It should allow for both "dot notation" and "bracket notation" to 
 - It should work well with both Polars and Pandas.
 
 Other ideas:
-- Also include the units used by cellpycore. Could be a good idea to have a object that 
+- Also include the units used by cellpycore. Could be a good idea to have a object that
 has convenient methods for converting between units, or make using e.g. pint easier for the coders.
 
 """
 
-from dataclasses import dataclass
 from enum import StrEnum, Enum
+import polars as pl
 
 
-@dataclass
-class ColsItem(str):
-    value: str
-    unit: str
-    dtype: str
-    def __str__(self) -> str:
-        return self.value
-    def __repr__(self) -> str:
-        return self.value
-    def __eq__(self, other: str) -> bool:
-        return self.value == other
-    def __hash__(self) -> int:
-        return hash(self.value)
-
-    
-
-
-
-class Cols(StrEnum):
+class SimpleCols(StrEnum):
     __version__: str = "1.0.0"
     A: str = "col1"
     B: str = "col2"
     C: str = "col3"
 
-    def is_valid(self, value: str) -> bool:
-        return value in self.__members__
 
-
-class SuperDuperCols(Enum):
-
-    __version__: str = "1.0.0"
-
-    def __new__(cls, value: str, unit: str, dtype: str):
-        obj = object.__new__(cls)
+class SuperDuperColsBase(str, Enum):
+    # problems with this: not clear how to extend the class with new members programmatically
+    def __new__(
+        cls, value: str, unit: str | None = None, dtype: pl.DataType | None = None
+    ):
+        obj = str.__new__(cls, value)
         obj._value_ = value
+        # consider converting unit to a pint unit object:
         obj.unit = unit
+        # consider converting to pl.DataType object if it is a string:
         obj.dtype = dtype
         return obj
-
-    # TODO: dtype should be a python native "dtype object" if it exists, or a pl.DataType? Using str for now.
-    #       It is also possible to convert from str to pl.DataType in the __new__ method, I guess.
-    A: list[str, str, str] = "col1", "V", "Float64"
-    B: list[str, str, str] = "col2", "mAh/g", "Float64"
-    C: list[str, str, str] = "col3", "s", "Float64"
 
     @property
     def value(self) -> str:
         return self._value_
 
+    @property
+    def is_aux(self) -> bool:
+        if self._name_.lower().startswith("aux_"):
+            return True
+        return False
+
+    @classmethod
+    def to_dict(self) -> dict:
+        d = {}
+        for label, member in self.__members__.items():
+            value = str(member.value)
+            unit = str(member.unit) if member.unit is not None else None
+            dtype = str(member.dtype) if member.dtype is not None else None
+            d[label] = {
+                "value": value,
+                "unit": unit,
+                "dtype": dtype,
+            }
+        return d
+
+
+class SuperDuperCols(SuperDuperColsBase):
+    __version__: str = "0.0.1"
+
+    # TODO: dtype should be a python native "dtype object" if it exists, or a pl.DataType? Using str for now.
+    #       It is also possible to convert from str to pl.DataType in the __new__ method, I guess.
+    A: list[str, str | None, pl.DataType | None] = "col1", "V", pl.Float64
+    B: list[str, str | None, pl.DataType | None] = "col2", "mAh/g", pl.Float64
+    C: list[str, str | None, pl.DataType | None] = "col3", "s", pl.Float64
+    # Does it work if we don't provide the unit and dtype? YES!
+    D = "col4"
 
 
 def super_duper_cols_check():
     import pandas as pd
     import polars as pl
-    
-    print(80*"-")
+
+    print(80 * "-")
     print("CHECKING SuperDuperCols")
     print(f"SuperDuperCols.__version__: {SuperDuperCols.__version__}")
-    print(80*"-")
+    print(80 * "-")
+
+    print(f"{SuperDuperCols.to_dict()=}")
+    print(80 * "-")
+
+    print("Proper one:")
+    print(f"{SuperDuperCols.A=}")
+    print(f"{SuperDuperCols.A.value=}")
+    print(f"{SuperDuperCols.A.unit=}")
+    print(f"{SuperDuperCols.A.dtype=}")
+    print(f"{SuperDuperCols.A.is_aux=}")
+
+    print("Not so proper one:")
+    print(f"{SuperDuperCols.D=}")
+    print(f"{SuperDuperCols.D.value=}")
+    print(f"{SuperDuperCols.D.unit=}")
+    print(f"{SuperDuperCols.D.dtype=}")
 
     test_data = {
         "col1": [1, 2, 3],
         "col2": [4, 5, 6],
         "col3": [7, 8, 9],
     }
-    
+
     df = pl.DataFrame(test_data)
     df_pandas = pd.DataFrame(test_data)
-    
+
     print(df)
     print(df.schema)
-    
+
     print(f"{SuperDuperCols.__annotations__=}")
     print(f"{SuperDuperCols.A.__annotations__=}")
     print(f"{SuperDuperCols.A.__annotations__['A']=}")
-    
-    v_enum = df.select(pl.col(SuperDuperCols.A.value).min()).item()
-    v_enum_str = df.select(pl.col(SuperDuperCols["A"].value).min()).item()
+
+    v_enum = df.select(pl.col(SuperDuperCols.A).min()).item()
+    v_enum_str = df.select(pl.col(SuperDuperCols["A"]).min()).item()
     v_str = df.select(pl.col("col1").min()).item()
-    
+
     print(f"v_enum: {v_enum}")
     print(f"v_enum_str: {v_enum_str}")
     print(f"v_str: {v_str}")
-    
+
     assert v_enum == v_str
     assert v_enum == v_enum_str
-        
+
     print("All tests passed for polars")
-    
-    v_enum_pandas = df_pandas.loc[:, SuperDuperCols.A.value].min()
+
+    v_enum_pandas = df_pandas.loc[:, SuperDuperCols.A].min()
     v_str_pandas = df_pandas.loc[:, "col1"].min()
-    v_enum_str_pandas = df_pandas.loc[:, SuperDuperCols["A"].value].min()
+    v_enum_str_pandas = df_pandas.loc[:, SuperDuperCols["A"]].min()
     print(f"v_str: {v_str_pandas}")
     print(f"v_enum: {v_enum_pandas}")
     print(f"v_enum_str: {v_enum_str_pandas}")
     assert v_str_pandas == v_str
     assert v_enum_pandas == v_str_pandas
     assert v_enum_pandas == v_enum_str_pandas
-    
+
     print("All tests passed for pandas")
+    print()
+    print(80 * "-")
+    for var in SuperDuperCols.__members__:
+        print(var)
+
+    print(80 * "-")
+    print(vars(SuperDuperCols.A))
+    print(SuperDuperCols.A._name_)
 
 
 def simple_cols_check():
     import pandas as pd
     import polars as pl
 
-    print(80*"-")
+    print(80 * "-")
     print("CHECKING Cols")
-    print(f"Cols.__version__: {Cols.__version__}")
-    print(80*"-")
+    print(f"Cols.__version__: {SimpleCols.__version__}")
+    print(80 * "-")
 
     test_data = {
-    "col1": [1, 2, 3],
-    "col2": [4, 5, 6],
-    "col3": [7, 8, 9],
-}
+        "col1": [1, 2, 3],
+        "col2": [4, 5, 6],
+        "col3": [7, 8, 9],
+    }
 
-
-
-    print(f"{Cols["A"]=}")
-    print(f"{Cols.A=}")
-    print(f"{Cols("col1")=}")
+    print(f"{SimpleCols["A"]=}")
+    print(f"{SimpleCols.A=}")
+    print(f"{SimpleCols("col1")=}")
 
     df = pl.DataFrame(test_data)
     df_pandas = pd.DataFrame(test_data)
@@ -147,13 +175,13 @@ def simple_cols_check():
     print(df)
     print(df.schema)
 
-    print(f"Cols.__version__: {Cols.__version__}")
-    print(f"{Cols.__annotations__=}")
-    print(f"{Cols.A.__annotations__=}")
-    print(f"{Cols.A.__annotations__['A']=}")
+    print(f"Cols.__version__: {SimpleCols.__version__}")
+    print(f"{SimpleCols.__annotations__=}")
+    print(f"{SimpleCols.A.__annotations__=}")
+    print(f"{SimpleCols.A.__annotations__['A']=}")
 
-    v_enum = df.select(pl.col(Cols.A).min()).item()
-    v_enum_str = df.select(pl.col(Cols["A"]).min()).item()
+    v_enum = df.select(pl.col(SimpleCols.A).min()).item()
+    v_enum_str = df.select(pl.col(SimpleCols["A"]).min()).item()
     v_str = df.select(pl.col("col1").min()).item()
 
     print(f"v_enum: {v_enum}")
@@ -163,7 +191,7 @@ def simple_cols_check():
     assert v_enum == v_str
     assert v_enum == v_enum_str
 
-    v_key = df.select(pl.col(Cols("col1")).min()).item()
+    v_key = df.select(pl.col(SimpleCols("col1")).min()).item()
 
     print(f"v_key: {v_key}")
 
@@ -171,9 +199,9 @@ def simple_cols_check():
 
     print("All tests passed for polars")
 
-    v_enum_pandas = df_pandas.loc[:, Cols.A].min()
+    v_enum_pandas = df_pandas.loc[:, SimpleCols.A].min()
     v_str_pandas = df_pandas.loc[:, "col1"].min()
-    v_enum_str_pandas = df_pandas.loc[:, Cols["A"]].min()
+    v_enum_str_pandas = df_pandas.loc[:, SimpleCols["A"]].min()
     print(f"v_str: {v_str_pandas}")
     print(f"v_enum: {v_enum_pandas}")
     print(f"v_enum_str: {v_enum_str_pandas}")
@@ -185,5 +213,5 @@ def simple_cols_check():
 
 
 if __name__ == "__main__":
-    simple_cols_check()
+    # simple_cols_check()
     super_duper_cols_check()
